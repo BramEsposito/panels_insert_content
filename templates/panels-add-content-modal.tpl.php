08b6fee918769b2f4e89b0dd58b0161a309d72d1
Bram Esposito <bram@bramesposito.com>
2015-10-22 09:28:06 +0200

    adding documentation and adding 2 sections:
    - specials (everything not being Blocks, FPP's, Views, Tokens, etc)
    - create FPP content

--- templates/panels-add-content-modal.tpl.php
+++ templates/panels-add-content-modal.tpl.php
@@ -4,7 +4,8 @@
  * Template to control the add content modal.
  */
 ?>
+<?php print($messages); ?>
 <div style="height:100%" class="panels_insert_content">
 	<div id="listFilter"> <div class="refreshlink"><?php print($refreshlink);?></div></div>
-  	<?php print_r($insertlinks); ?>
+  	<?php print($insertlinks); ?>
 </div>
